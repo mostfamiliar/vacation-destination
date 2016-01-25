66355db685d6ceb8dc9ff88e754031bfde12185c
alissa paluska and torrence stratton <tlstratton@gmail.com>
2016-01-25 14:08:21 -0800

    Finish user logic

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,24 @@ function place(located, landmark, season) {
 place.prototype.vacationDestination = function() {
   return this.located + " " + this.landmark + " " + this.season;
 }
+
+
+$(document).ready(function() {
+  $("#new-destination").submit(function(event){
+    event.preventDefault();
+    var locatedInput = $("#locationInput").val();
+    var landmarkInput = $("#landmarkInput").val();
+    var seasonInput = $("#seasonInput").val();
+
+    var newDestination = place(locatedInput, landmarkInput, seasonInput);
+
+    $("ul#destinations").append("<li class='location'>" + locatedInput + "</li>");
+    $(".location").last().click(function() {
+    $("#show-output").show();
+    $(".locationOutput").text(" " + locatedInput);
+    $(".landmarkOutput").text(" " + landmarkInput);
+    $(".seasonOutput").text(" " + seasonInput);
+    $("p.destinationList").text(newDestination);
+  });
+ });
+});
